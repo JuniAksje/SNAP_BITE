3905201879862a27108935746a665bc9b6be12b6
JosÃe Feliciano <jr.feliciano98@gmail.com>
2025-12-02 14:34:36 -0300

    primeiro push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def ask
+    @question = params[:question]
+
+    @answer = "Essa seria a resposta da IA para: #{@question}"
+
+    render :home
+  end
 end
