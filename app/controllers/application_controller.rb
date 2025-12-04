48dc4c433a15d1ffcce81ed0e89cb11d71cea216
andre rezende <andre_fmr89@hotmail.com>
2025-12-04 16:20:40 -0300

    seed atualizado, sidebar e estilo,logica de background image da sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_restaurants
+  before_action :set_restaurants_sidebar
 
   private
 
   def set_restaurants
     @restaurants = Restaurant.all
+  end
+
+  def set_restaurants_sidebar
+    @city_backgrounds = {
+    "São Paulo" => "sao_paulo.jpg",
+    "Rio de Janeiro" => "rio.jpg",
+    "Belo Horizonte" => "bh.jpg",
+    "Salvador" => "salvador.jpg",
+    "Porto Alegre" => "porto_alegre.jpg",
+    "Recife" => "recife.jpg",
+    "Fortaleza" => "fortaleza.jpg",
+    "Curitiba" => "curitiba.jpg",
+    "Manaus" => "manaus.jpg",
+    "Brasília" =>"brasilia.jpg"
+    }
   end
 end
